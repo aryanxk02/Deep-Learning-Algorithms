86e61c2f0d20dabff7a098df97e75d611b296a84
Aryan Kenchappagol <59761275+aryanxk02@users.noreply.github.com>
2022-08-28 00:57:26 +0530

    add AlextNet architecture

--- Multilabel Classification/Movie genre Classification.py
+++ Multilabel Classification/Movie genre Classification.py
@@ -1,9 +1,10 @@
-# import libraries
+#imports
 from keras.preprocessing import image
 from keras.model import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import BatchNormalization
+from keras import Sequential
 
 from tqdm import tqdm
 
@@ -13,19 +14,72 @@ import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 
-# set dataset path
-image_directory = 'Users/aryan/Desktop/DL/movie_data'
 
-df = pd.read_csv('movie_metadata.csv')
+image_directory = 'Users/aryan/Desktop/DL/movie-data'
+
+df = pd.read_csv('movie-metadata.csv')
+df.head()
+
+df.columns
 df.shape
 
 SIZE = 250
 X = []
+count = 0
 
 for i in tqdm(range(df.shape[0])):
-    img = image.load_image(image_directory+df['id'][i]+'.jpg', target_size=(SIZE, SIZE))
+    try:
+        img = tf.keras.utils.load_img(image_directory+'/'+df['Id'][i]+'.jpg', target_size=(SIZE, SIZE))
+    except:
+        continue
     img = image.img_to_array(img)
     img = img/255
     X.append(img)
+    count += 1
     
-X = np.array(x)
+X = np.array(X)
+y = np.array(df.drop(['Id', 'Genre'], axis=1))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=20, test_size=0.3)
+
+# AlexNet
+
+import keras
+import keras.layers as layers
+
+model = keras.Sequential()
+
+model = keras.Sequential()
+model.add(layers.Conv2D(filters=96, kernel_size=(11, 11), 
+                        strides=(4, 4), activation="relu", 
+                        input_shape=(227, 227, 3)))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPool2D(pool_size=(3, 3), strides= (2, 2)))
+model.add(layers.Conv2D(filters=256, kernel_size=(5, 5), 
+                        strides=(1, 1), activation="relu", 
+                        padding="same"))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)))
+model.add(layers.Conv2D(filters=384, kernel_size=(3, 3), 
+                        strides=(1, 1), activation="relu", 
+                        padding="same"))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(filters=384, kernel_size=(3, 3), 
+                        strides=(1, 1), activation="relu", 
+                        padding="same"))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(filters=256, kernel_size=(3, 3), 
+                        strides=(1, 1), activation="relu", 
+                        padding="same"))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(4096, activation="relu"))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(10, activation="softmax"))
+model.compile(loss='sparse_categorical_crossentropy', 
+              optimizer=tf.optimizers.SGD(lr=0.001), 
+              metrics=['accuracy'])
+model.summary()
+
+model.compile(optimizer='adam', oss='binary_crossentropy', metrics=['accuracy'])
